48b87066afbb5fdad8a402ed6f6321283c9bd103
locxter <54595101+locxter@users.noreply.github.com>
2021-05-03 14:44:45 +0200

    Updated the installation script.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "Started changing the Inkscape configuration."
 cp default.svg ~/.config/inkscape/templates/default.svg
-echo "Changed the default template successfully."
+echo "Changed the Inkscape configuration successfully."
